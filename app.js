69a9e7d56681fc27dd2748c9f9ffd620f8ee495c
dgreatorex85 <131242920+dgreatorex85@users.noreply.github.com>
2023-04-19 10:42:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -45,13 +45,14 @@ function App() {
   }
 
 const svg = (
-  '<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">' +
-    '<circle cx="100" cy="100" r="100" fill="' + circleColor + '" />' +
-    '<text x="50%" y="50%" text-anchor="middle" dy=".3em" font-family="' + font + '" font-size="100" fill="' + charColor + '">' +
-      character +
-    '</text>' +
-  '</svg>'
+  `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="100" cy="100" r="100" fill="${circleColor.replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}" />
+    <text x="50%" y="50%" text-anchor="middle" dy=".3em" font-family="${font.replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}" font-size="100" fill="${charColor.replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}">
+      ${character.replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}
+    </text>
+  </svg>`
 );
+
 
 
   return (
