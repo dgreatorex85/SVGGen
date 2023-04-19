7adec7cd846cd1edc83c8d8d16864ea68b83e17b
dgreatorex85 <131242920+dgreatorex85@users.noreply.github.com>
2023-04-19 11:02:13 +0100

    Create IconSVG.js

---
+++ IconSVG.js
@@ -0,0 +1,20 @@
+function IconSVG({ character, font, charColor, circleColor }) {
+  return (
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="100" cy="100" r="100" fill={circleColor} />
+      <text
+        x="50%"
+        y="50%"
+        text-anchor="middle"
+        dy=".3em"
+        font-family={font}
+        font-size="100"
+        fill={charColor}
+      >
+        {character}
+      </text>
+    </svg>
+  );
+}
+
+export default IconSVG;
