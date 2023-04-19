dda451aa87a1c88a728335082ea5e6251c98df0e
dgreatorex85 <131242920+dgreatorex85@users.noreply.github.com>
2023-04-19 10:40:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,14 +44,15 @@ function App() {
     link.click();
   }
 
-  const svg = (
-    `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="100" cy="100" r="100" fill="${circleColor}" />
-      <text x="50%" y="50%" text-anchor="middle" dy=".3em" font-family="${font}" font-size="100" fill="${charColor}">
-        ${character}
-      </text>
-    </svg>`
-  );
+const svg = (
+  '<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">' +
+    '<circle cx="100" cy="100" r="100" fill="' + circleColor + '" />' +
+    '<text x="50%" y="50%" text-anchor="middle" dy=".3em" font-family="' + font + '" font-size="100" fill="' + charColor + '">' +
+      character +
+    '</text>' +
+  '</svg>'
+);
+
 
   return (
     <div className={Container}>
