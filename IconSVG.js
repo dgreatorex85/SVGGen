bb5a5c849c34a50d60ef123f624a402976e22864
dgreatorex85 <131242920+dgreatorex85@users.noreply.github.com>
2023-04-19 11:13:25 +0100

    Update IconSVG.js

--- IconSVG.js
+++ IconSVG.js
@@ -1,6 +1,6 @@
-function IconSVG({ character, font, charColor, circleColor }) {
+function IconSVG({ id, character, font, charColor, circleColor }) {
   return (
-    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <svg id={id} width="200" height="200" xmlns="http://www.w3.org/2000/svg">
       <circle cx="100" cy="100" r="100" fill={circleColor} />
       <text
         x="50%"
